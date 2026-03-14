17271f480dc1eea3e54736cee55d09212328cd18
Dr Sajjad Anwar <hellosajjad@outlook.com>
2026-03-15 01:56:49 +0500

    footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -3,5 +3,5 @@
 ?>
 <footer class="footer center-align" style="padding: 20px 0; background: transparent;">
   <hr style="width: 50%; background-color: #fff; height: 2px; border: none; margin: 20px auto;">
-  <p>&copy; <?php echo date("Y"); ?> anwar.health (smc-private) limited</p>
+  <p>&copy; <?php echo date("Y"); ?> hospital0</p>
 </footer>
